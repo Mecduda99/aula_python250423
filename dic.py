65141d002868336a955d24075e9430714850c710
mecduda99 <rm98760@fiap.com.br>
2023-04-25 22:58:38 -0300

    "iniciando o conteúdo de dicionários"

---
+++ dic.py
@@ -0,0 +1,15 @@
+# dicionario = {"dado":{"Objeto de 6 lados com numeros engravados em cada uma das faces"}, "Virgula": {"sinal ortográfico de separação de blablabla"}}
+# identificador + lista
+
+produtos = {1:["Banana", 20,3,12345,1.0,"18/03/2023","",1,"Copa","São Paulo"], 2: ["Notebook", 20,3,12345,1.0,"18/03/2023","",1,"Copa","São Paulo"]}
+
+for index, data in produtos.items():
+    print(index)
+    print(data[0])
+
+
+
+# def buscar():
+#     print(dicionario[input("entre com a palavra: ")])
+
+# buscar()
